683242555ac6379e77e9fa80f1d02476d5dab6d4
NaoshiAC <naoshiarimori@gmail.com>
2021-03-20 16:49:08 -0300

    Implemented Components Context and Sidebar
    - In order to achieve the Ignite Challenge, I've implemented the Context and SideBar components.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+
+import { MovieProps } from '../App';
+import { MovieCard } from './MovieCard';
+import { GenreResponseProps } from './SideBar';
+
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
